eb41256b001d0714bf988971bfb84437cff88406
jakob <jakob.spiess@stud.unibas.ch>
2025-04-10 18:13:23 +0200

    ended the js code, finish the project

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function getComputerChoice() {
 }
 function getHumanChoice() {
     let humanChoice = prompt("What is your choice? If it is not known we will use scissors.");
+    humanChoice = humanChoice.toLowerCase();
     if (humanChoice === "rock") {
         return humanChoice;
     } else if (humanChoice === "paper") {
@@ -20,4 +21,45 @@ function getHumanChoice() {
         return "scissors";
     }
 }
-console.log((getHumanChoice()));
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+
+        console.log(`Computer chose ${computerChoice}`);
+        console.log(`Human chose ${humanChoice}`);
+
+        if (humanChoice === computerChoice) {
+            console.log("It's a tie");
+        }
+        else if (((humanChoice === "rock") && (computerChoice === "scissors")) ||
+                ((humanChoice === "paper") && (computerChoice === "rock")) ||
+                ((humanChoice === "scissors") && (computerChoice === "paper"))) {
+                    humanScore++;
+                    console.log("Human wins");
+        } else {
+            computerScore++;
+            console.log("Computer wins");
+        }
+        console.log(`Human score ${humanScore}, Computer Score: ${computerScore}`);
+    }
+
+    let humanChoice;
+    let computerChoice;
+    for(let i = 0; i < 5; i++) {
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    if(humanScore > computerScore) {
+        console.log("The human wins the game.");
+    } else if (computerScore > humanScore) {
+        console.log("The computer wins the game.");
+    } else {
+        console.log("The game is a tie.")
+    }
+}
+
+playGame();
