5af86ae4b62d9a80a4d0348619a2ee0803d617a5
jakob <jakob.spiess@stud.unibas.ch>
2025-04-26 12:30:09 +0200

    A very simple html UI completed with JS dom methods.

--- script.js
+++ script.js
@@ -28,31 +28,100 @@ function playGame() {
 
     function playRound(humanChoice, computerChoice) {
 
-        console.log(`Computer chose ${computerChoice}`);
-        console.log(`Human chose ${humanChoice}`);
+        //console.log(`Computer chose ${computerChoice}`);
+        //console.log(`Human chose ${humanChoice}`);
 
+        const currIter = document.createElement('ul');
+        const compChoice = document.createElement('li');
+        const humChoice = document.createElement('li');
+        compChoice.textContent = `Computer chose ${computerChoice}`;
+        humChoice.textContent = `Human chose ${humanChoice}`;
+        currIter.appendChild(compChoice);
+        currIter.appendChild(humChoice);
+
+        let textHolder;
         if (humanChoice === computerChoice) {
-            console.log("It's a tie");
+            textHolder = document.createElement('li');
+            textHolder.textContent = "It's a tie";
+            currIter.appendChild(textHolder);
+            //console.log("It's a tie");
         }
         else if (((humanChoice === "rock") && (computerChoice === "scissors")) ||
                 ((humanChoice === "paper") && (computerChoice === "rock")) ||
                 ((humanChoice === "scissors") && (computerChoice === "paper"))) {
                     humanScore++;
-                    console.log("Human wins");
+                    textHolder = document.createElement('li');
+                    textHolder.textContent = "Human wins";
+                    currIter.appendChild(textHolder);
+                    //console.log("Human wins");
         } else {
             computerScore++;
-            console.log("Computer wins");
+            textHolder = document.createElement('li');
+            textHolder.textContent = "Computer wins";
+            currIter.appendChild(textHolder);
+            //console.log("Computer wins");
         }
-        console.log(`Human score ${humanScore}, Computer Score: ${computerScore}`);
+        textHolder = document.createElement('li');
+        textHolder.textContent = `Human score ${humanScore}, Computer Score: ${computerScore}`;
+        currIter.appendChild(textHolder);
+        //console.log(`Human score ${humanScore}, Computer Score: ${computerScore}`);
+        let winnerDisplay;
+        if (humanScore === 5) {
+            winnerDisplay = document.createElement('h3');
+            winnerDisplay.innerText = "The winner is the human!";
+            currIter.appendChild(winnerDisplay);
+        } else if (computerScore === 5) {
+            winnerDisplay = document.createElement('h3');
+            winnerDisplay.innerText = "The winner is the computer!";
+            currIter.appendChild(winnerDisplay);
+        }
+        results.appendChild(currIter);
     }
 
     let humanChoice;
     let computerChoice;
-    for(let i = 0; i < 5; i++) {
-        humanChoice = getHumanChoice();
+    
+    //Instead of automatic methods get buttons & listeners
+    //humanChoice = getHumanChoice();
+    const menu = document.createElement('div');
+    const rockBtn = document.createElement('button');
+    rockBtn.setAttribute("id", "rock");
+    const paperBtn = document.createElement('button');
+    paperBtn.setAttribute("id", "paper"); 
+    const scissorsBtn = document.createElement('button');
+    scissorsBtn.setAttribute("id", "scissors");
+    rockBtn.textContent = "ROCK";
+    paperBtn.textContent = "PAPER";
+    scissorsBtn.textContent = "SCISSORS";
+    menu.appendChild(rockBtn);
+    menu.appendChild(paperBtn);
+    menu.appendChild(scissorsBtn);
+    const body = document.querySelector("body");
+    body.appendChild(menu);
+
+    const results = document.createElement('div');
+    results.setAttribute("id", "results");
+    body.appendChild(results);
+
+    menu.addEventListener('click', (e) => {
+        let target = e.target;
+
+        switch(target.id) {
+            case "rock":
+                humanChoice = "rock";
+                break;
+            case "paper":
+                humanChoice = "paper";
+                break;
+            case "scissors":
+                humanChoice = "scissors";
+                break;
+        }
         computerChoice = getComputerChoice();
         playRound(humanChoice, computerChoice);
-    }
+    });
+    
+    /*
     if(humanScore > computerScore) {
         console.log("The human wins the game.");
     } else if (computerScore > humanScore) {
@@ -60,6 +129,7 @@ function playGame() {
     } else {
         console.log("The game is a tie.")
     }
+    */
 }
 
 playGame();
